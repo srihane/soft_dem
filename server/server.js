bc76d97fb5accc67331f5e054dc4828b82310f52
Desaser <rihane.samy@gmail.com>
2022-10-03 20:43:50 +0200

    Init

--- server/server.js
+++ server/server.js
@@ -15,7 +15,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const cors = require("cors");
-app.use(cors());
+//app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "https://demasoft.studyneo.com" }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 // // MYSQL
 const mysql = require("mysql");
